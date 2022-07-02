7e7ab44eb673d9f583fc4d7a186205793d68b1b5
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-02 21:39:35 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Копия массива с помощью элементарного копирования
+
+
+int[] array = {1, 3, 5, 9, -6, 5, 0};
+int[] copy = new int[array.Length];
+
+for (int i = 0; i < copy.Length; i++)
+{
+    copy[i] = array[i];
+    Console.Write(copy[i] + " ");
+}
